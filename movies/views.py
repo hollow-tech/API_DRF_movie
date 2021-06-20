be53817a02e6e053ae96fa05dc406187e5561c3c
hollow <hollowtechmail@gmail.com>
2021-06-21 00:12:06 +0600

    The first commit

---
+++ movies/views.py
@@ -0,0 +1,60 @@
+from rest_framework import generics
+from django.db import models
+
+from .models import Movie, Actor
+from .serializers import (
+    MovieListSerializer,
+    MovieDetailSerializer,
+    ReviewCreateSerializer,
+    CreateRatingSerializer,
+    ActorListSerializer,
+    ActorDetailSerializer,
+)
+from .service import get_client_ip
+
+
+class MovieListView(generics.ListAPIView):
+    """Вывод списка фильмов"""
+    serializer_class = MovieListSerializer
+
+    def get_queryset(self):
+        movies = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count("ratings",
+                                     filter=models.Q(ratings__ip=get_client_ip(self.request)))
+        ).annotate(
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
+        return movies
+
+
+class MovieDetailView(generics.RetrieveAPIView):
+    """Вывод фильма"""
+    queryset = Movie.objects.filter(draft=False)
+    serializer_class = MovieDetailSerializer
+
+
+class ReviewCreateView(generics.CreateAPIView):
+    """Добавление отзыва к фильму"""
+
+    serializer_class = ReviewCreateSerializer
+
+
+class AddStarRatingView(generics.CreateAPIView):
+    """Добавление рейтинга фильму"""
+
+    serializer_class = CreateRatingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(ip=get_client_ip(self.request))
+
+
+class ActorsListView(generics.ListAPIView):
+    """Вывод списка актеров"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorListSerializer
+
+
+class ActorsDetailView(generics.RetrieveAPIView):
+    """Вывод актера или режиссера"""
+    queryset = Actor.objects.all()
+    serializer_class = ActorDetailSerializer
